1413dee4ed63187eb82a28637f185631551391e4
eoblue <eoblue22@naver.com>
2024-05-19 18:04:02 +0900

    table 추가 - property

--- src/main/resources/sql/ssafyhome.sql
+++ src/main/resources/sql/ssafyhome.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f93e24795b98a1cbee7c30bc0c49f86362d2f012bd1fb92072a688f2c16b58f6
-size 313419452
+oid sha256:a9c1b286040ffe47d032f183bd8cd942ec191e9fcd6d48ef450a350927d82f8f
+size 313420049
